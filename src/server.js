a3ffd259a0ec9f8a9aa6634743dcd6cca4b61e0f
rabbit-97 <uzam97@gmail.com>
2024-10-28 20:55:55 +0900

    feat: initServer 선언 후 적용
     - 서버 실행 시 initServer 실행하기 위한 로직 추가

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 import net from 'net';
 import { HOST, PORT } from './constants/env.js';
 import { onConnection } from './events/onConnection.js';
+import initServer from './init/index.js';
 
 const server = net.createServer(onConnection);
 
-server.listen(PORT, HOST, () => {
-  console.log(`Server is running at ${HOST}:${PORT}`);
-});
+initServer()
+  .then(() => {
+    server.listen(PORT, HOST, () => {
+      console.log(`Server is running at ${HOST}:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
