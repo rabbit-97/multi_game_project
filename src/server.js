927dd15d82a38b6ea0738b06bd031e82c5f5ca53
rabbit-97 <uzam97@gmail.com>
2024-10-28 18:37:09 +0900

    feat: 이벤트 생성

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import net from 'net';
 import { HOST, PORT } from './constants/env.js';
+import { onConnection } from './events/onConnection.js';
 
-const server = net.createServer();
+const server = net.createServer(onConnection);
 
 server.listen(PORT, HOST, () => {
   console.log(`Server is running at ${HOST}:${PORT}`);
