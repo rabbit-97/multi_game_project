e2cc713d619b4a35453836b20b3e5bd321273216
rabbit-97 <uzam97@gmail.com>
2024-10-30 16:28:24 +0900

    rename: 이름 수정

--- src/sessions/game.session.js
+++ src/sessions/game.session.js
@@ -1,16 +1,16 @@
 import Game from '../class/models/game.class.js';
-import { gameSession } from './sessions.js';
+import { gameSessions } from './sessions.js';
 
 export const addGameSession = (id) => {
   const session = new Game(id);
-  gameSession.push(session);
+  gameSessions.push(session);
   return session;
 };
 
 export const removeGameSession = () => {
-  delete gameSession[0];
+  delete gameSessions[0];
 };
 
 export const getGameSession = () => {
-  return gameSession[0];
+  return gameSessions[0];
 };
