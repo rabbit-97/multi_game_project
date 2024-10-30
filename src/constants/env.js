773e6b059fb309760ce405e4245bc75781c901e8
rabbit-97 <uzam97@gmail.com>
2024-10-30 14:41:09 +0900

    fix: 오류 수정
     - 에러 메세지 : Error: Invalid client version

--- src/constants/env.js
+++ src/constants/env.js
@@ -4,4 +4,4 @@ dotenv.config();
 
 export const HOST = process.env.HOST || 'localhost';
 export const PORT = process.env.PORT || '5555';
-export const CLIENT_VERSION = process.env.PORT || '1.0.0';
+export const CLIENT_VERSION = process.env.CLIENT_VERSION || '1.0.0';
