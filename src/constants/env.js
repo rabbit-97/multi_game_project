69a655f0882c388178861fe3d818a28554cfaf41
rabbit-97 <uzam97@gmail.com>
2024-10-31 22:31:33 +0900

    fix: rds 연결

--- src/constants/env.js
+++ src/constants/env.js
@@ -9,5 +9,6 @@ export const CLIENT_VERSION = process.env.CLIENT_VERSION || '1.0.0';
 export const DB_NAME = process.env.DB_NAME || 'multi_game_project';
 export const DB_USER = process.env.DB_USER || 'root';
 export const DB_PASSWORD = process.env.DB_PASSWORD || 'Kws!1274414';
-export const DB_HOST = process.env.DB_HOST || '127.0.0.1';
+export const DB_HOST =
+  process.env.DB_HOST || 'express-database.c3s0ieiu613c.ap-northeast-2.rds.amazonaws.com';
 export const DB_PORT = process.env.DB_PORT || '3306';
